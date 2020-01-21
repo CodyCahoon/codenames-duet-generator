735419d7222617923f58ef9721f4c6a1ea1c7d63
Cody Cahoon <codyjcahoon@gmail.com>
2020-01-21 16:08:39 -0500

    Begin implementation using ramda

---
+++ src/ramda.js
@@ -0,0 +1,37 @@
+const R = require('ramda');
+const Util = require('./utilities');
+
+R.compose(
+  R.pipe(sides => {}),
+  R.pipe(sideA => {
+    return {
+      sideA: {
+        ...sideA
+      },
+      sideB: {
+        green: R.unnest([
+          R.slice(0, 3, sideA.green),
+          R.slice(0, 1, sideA.black),
+          R.slice(0, 5, sideA.white)
+        ]),
+        black: R.unnest([
+          R.slice(3, 4, sideA.green),
+          R.slice(1, 2, sideA.black),
+          R.slice(5, 6, sideA.white)
+        ]),
+        white: R.unnest([
+          R.slice(4, 9, sideA.green),
+          R.slice(2, 3, sideA.black),
+          R.slice(6, Infinity, sideA.white)
+        ])
+      }
+    };
+  }),
+  R.pipe(nums => {
+    return {
+      green: R.slice(0, 9, nums),
+      black: R.slice(9, 12, nums),
+      white: R.slice(13, Infinity, nums)
+    };
+  })
+)(Util.randomNums(25));
